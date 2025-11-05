b3f9b01ae8ed0f15b40ac96e978b1ced886c0e7a
SannikovVladislav <sannikovladislav@mail.ru>
2025-11-06 02:48:07 +0800

    Initial Commit

---
+++ CreatureCards/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CreatureCards
+//
+//  Created by Владислав on 06.11.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
